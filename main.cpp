0a6af1e8df59319f814a31ea2f6f578f484cb28f
integraldx <jmh121463@gmail.com>
2019-03-06 22:09:29 +0900

    Being ready for glfw abstraction

--- main.cpp
+++ main.cpp
@@ -1,45 +1,37 @@
 #include <iostream>
 #include <GL/gl.h>
+#include <GL/glu.h>
 #include "Includes/GLFW/glfw3.h"
+#include "windowControl.hpp"
 
 int main()
 {
-    GLFWwindow* window;
-
-    if (!glfwInit())
-    {
-        return -1;
-    }
-
-    window = glfwCreateWindow(1280, 960, "Hello World!", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
+    GLFWwindow* window = SNUCG::initGlfwEnviornment(1280, 960, "Hello World!");
 
     glClearColor (0.0, 0.0, 0.0, 0.0);
     glClear (GL_COLOR_BUFFER_BIT);
     glColor3f (1.0, 1.0, 1.0);
     glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
     glBegin(GL_POLYGON);
+    {
         glVertex3f (0.25, 0.25, 0.0);
         glVertex3f (0.75, 0.25, 0.0);
         glVertex3f (0.75, 0.75, 0.0);
         glVertex3f (0.25, 0.75, 0.0);
+    }
     glEnd();
     glFlush();
 
-    while (!glfwWindowShouldClose(window))
+    GLuint VertexArrayID;
+    
+
+    while (!glfwWindowShouldClose(window) && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS)
     {
         glfwSwapBuffers(window);
 
         glfwPollEvents();
     }
-    glfwTerminate();
-    return 0;
 
+    SNUCG::endGlfwEnviornment();
     return 0;
 }
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
-main: main.cpp
+main: main.cpp windowControl.hpp
 	g++ main.cpp Binaries/libglfw3.a -lGL -ldl -lX11 -lXrandr -lXi -lXext -lXcursor -lXxf86vm -lXinerama -lpthread -o main
