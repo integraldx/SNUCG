7e3af37e83998fa2384a9a8f250292e933974204
integraldx <jmh121463@gmail.com>
2019-03-20 23:23:46 +0900

    Object implementation

--- Object.cpp
+++ Object.cpp
@@ -1,13 +1,72 @@
 #include "Object.hpp"
 
+Object::Object()
+{
+    vertices.push_back({0.5, 0.5, 0});
+    vertices.push_back({0.5, -0.5, 0});
+    vertices.push_back({-0.5, -0.5, 0});
+    vertices.push_back({-0.5, 0.5, 0});
+}
+
+Object::Object(std::vector<Vec3> vertices)
+{
+    for(Vec3 v : vertices)
+    {
+        this->vertices.push_back(v);
+    }
+}
+
+Object::~Object()
+{
+
+}
+
 void Object::draw()
 {
-    glColor3f(color.x, color.y, color.z);
-    glBegin(GL_POLYGON);
+    glPushMatrix();
     {
-        for(Vec3 v : vertices)
+        glRotatef(rotationAngle, rotationAxis.x, rotationAxis.y, rotationAxis.z);
+        glTranslatef(position.x, position.y, position.z);
+        glColor3f(color.x, color.y, color.z);
+        glBegin(GL_POLYGON);
         {
-            glVertex3f(v.x, v.y, v.z);
+            for(Vec3 v : vertices)
+            {
+                glVertex3f(v.x, v.y, v.z);
+            }
+        }
+        glEnd();
+        for(auto& o : childs)
+        {
+            o->draw();
         }
     }
+    glPopMatrix();
+    return;
+}
+
+void Object::setPosition(Vec3 v)
+{
+    position = v;
+}
+
+void Object::setRotation(float f, Vec3 v)
+{
+    rotationAxis = v;
+    rotationAngle = f;
+}
+
+void Object::setScale(Vec3 v)
+{
+    scale = v;
+}
+
+void Object::setColor(Vec3 v)
+{
+    color = v;
+}
+
+void Object::addChild(std::unique_ptr<Object> obj)
+{
+    childs.push_back(std::move(obj));
 }
--- Object.hpp
+++ Object.hpp
@@ -2,18 +2,30 @@
 
 #include <vector>
 #include "Util.hpp"
-#include <gl/GL.h>
-#include <gl/GLU.h>
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <memory>
 
 class Object
 {
 private:
     std::vector<Vec3> vertices;
-    Vec3 color;
+    Vec3 position = {0, 0, 0};
+    Vec3 rotationAxis = {0, 0, 0};
+    float rotationAngle = 0;
+    Vec3 scale = {1, 1, 1};
+
+    Vec3 color = {1, 1, 1};
+    std::vector<std::unique_ptr<Object>> childs;
 
 public:
+    Object();
+    Object(std::vector<Vec3> vertices);
+    ~Object();
     void draw();
-
-    
-
+    void setPosition(Vec3);
+    void setRotation(float, Vec3);
+    void setScale(Vec3);
+    void setColor(Vec3);
+    void addChild(std::unique_ptr<Object>);
 }
