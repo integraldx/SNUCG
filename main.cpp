9d271e84c53a935f37d9730a9fc7547c6050938e
Integraldx <jmh121463@gmail.com>
2019-05-01 21:19:11 +0900

    Input format refine

--- main.cpp
+++ main.cpp
@@ -4,10 +4,13 @@ int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
 
-    std::string s;
-    std::cin >> s;
+    if(argc < 2)
+    {
+        std::cout << "Usage: ./main [input file name]" << std::endl;
+        return 0;
+    }
 
-    SceneManager::initializeScene(s);
+    SceneManager::initializeScene(std::string(argv[1]));
     glutMainLoop();
     return 0;  
 }
